8e3dda494ad2ec2754cbee831a817ea675dd15cd
Sifat Musfique <sifatmusfique@gmail.com>
2026-02-10 03:55:10 -0800

    Updated project details

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,75 +19,86 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@graph": [
+      {
+        "@type": "Person",
+        "@id": "https://sifatmusfique.dev/#person",
+        "url": "https://sifatmusfique.dev/",
+        "mainEntityOfPage": "https://sifatmusfique.dev/",
+        "name": "Sifat Musfique",
+        "jobTitle": "Software Developer",
+        "description": "Sifat Musfique is a software developer and researcher based in Bangladesh. He is a student of Computer Science and Engineering at Varendra University focusing on web infrastructure and AI-driven technologies.",
+        "nationality": "Bangladeshi",
+        "affiliation": {
+          "@type": "EducationalOrganization",
+          "name": "Varendra University",
+          "url": "https://vu.edu.bd/"
+        },
+        "brand": {
+          "@type": "Brand",
+          "name": "TensorTide"
+        },
+        "knowsAbout": ["React", "PHP", "Web Development", "Chess Strategy", "AI-driven technologies"],
+        "homeLocation": {
+          "@type": "Place",
+          "name": "Rajshahi, Bangladesh"
+        },
+        "address": {
+          "@type": "PostalAddress",
+          "addressLocality": "Rajshahi",
+          "postalCode": "6000",
+          "addressCountry": "BD"
+        },
+        "contactPoint": {
+          "@type": "ContactPoint",
+          "email": "sifatmusfique@gmail.com",
+          "telephone": "+8801560009992",
+          "contactType": "customer service"
+        },
+        "sameAs": [
+          "https://www.google.com/search?kgmid=/g/11yzdbf2km",
+          "https://wikialpha.co/wiki/Sifat_Musfique",
+          "https://www.crunchbase.com/person/sifat-musfique",
+          "https://www.wikidata.org/wiki/Q138035556",
+          "https://ratings.fide.com/profile/315105339",
+          "https://scholar.google.com/citations?user=LsO3KMsAAAAJ",
+          "https://www.researchgate.net/profile/Sifat-Musfique",
+          "https://github.com/sifatmusfique",
+          "https://www.linkedin.com/in/sifatmusfique-dev",
+          "https://dev.to/sifatmusfique",
+          "https://hashnode.com/@sifatmusfique",
+          "https://stackoverflow.com/users/32314667/sifat-musfique",
+          "https://codepen.io/sifatmusfique",
+          "https://www.fiverr.com/sifatmusfique",
+          "https://www.xing.com/profile/Sifat_Musfique",
+          "https://about.me/sifatmusfique",
+          "https://www.ted.com/profiles/51097968",
+          "https://gravatar.com/sifatmusfique",
+          "https://www.facebook.com/sifatmusfique",
+          "https://www.instagram.com/sifat.musfique",
+          "https://www.skool.com/@sifat-musfique-8682"
+        ]
+      }
+    ]
+  };
+
   return (
     <html lang="en" className="dark overflow-x-hidden">
-      <body className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden text-gray-200`}>
+      <head>
         <script
           type="application/ld+json"
-          dangerouslySetInnerHTML={{
-            __html: JSON.stringify({
-              "@context": "https://schema.org",
-              "@type": "Person",
-              "name": "Sifat Musfique",
-              "url": "https://sifatmusfique.dev/",
-              "@id": "https://www.google.com/search?kgmid=/g/11yzdbf2km",
-              "jobTitle": "Software Developer",
-              "knowsAbout": ["React", "PHP", "Web Development", "Chess Strategy"],
-              "nationality": "Bangladeshi",
-              "homeLocation": {
-                "@type": "Place",
-                "name": "Rajshahi, Bangladesh"
-              },
-              "affiliation": {
-                "@type": "EducationalOrganization",
-                "name": "Varendra University"
-              },
-              "brand": {
-                "@type": "Brand",
-                "name": "TensorTide"
-              },
-              "knowsAbout": ["React", "PHP", "Web Development", "Chess Strategy"],
-              "sameAs": [
-                "https://www.crunchbase.com/person/sifat-musfique",
-                "https://wikialpha.co/wiki/Sifat_Musfique",
-                "https://www.wikidata.org/wiki/Q138035556",
-                "https://scholar.google.com/citations?user=LsO3KMsAAAAJ",
-                "https://ratings.fide.com/profile/315105339",
-                "https://www.linkedin.com/in/sifatmusfique-dev",
-                "https://github.com/sifatmusfique",
-                "https://dev.to/sifatmusfique",
-                "https://www.xing.com/profile/Sifat_Musfique",
-                "https://about.me/sifatmusfique",
-                "https://www.fiverr.com/sifatmusfique",
-                "https://codepen.io/sifatmusfique",
-                "https://www.facebook.com/sifatmusfique",
-                "https://www.instagram.com/sifat.musfique",
-                "https://www.skool.com/@sifat-musfique-8682",
-                "https://stackoverflow.com/users/32314667/sifat-musfique",
-                "https://hashnode.com/@sifatmusfique",
-                "https://www.ted.com/profiles/51097968",
-                "https://gravatar.com/sifatmusfique"
-              ],
-              "contactPoint": {
-                "@type": "ContactPoint",
-                "email": "sifatmusfique@gmail.com",
-                "telephone": "+8801560009992",
-                "contactType": "customer service"
-              },
-              "address": {
-                "@type": "PostalAddress",
-                "addressLocality": "Rajshahi",
-                "postalCode": "6000",
-                "addressCountry": "BD"
-              }
-            })
-          }}
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
+      </head>
+      <body className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden text-gray-200`}>
         <SmoothScroll />
         <BackgroundCanvas />
         <Navbar />
         <AosInit />
-        <AIAgent />{children}
+        <AIAgent />
+        {children}
         <Footer />
       </body>
     </html>
